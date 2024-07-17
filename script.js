6af37f844e2fc3f26d689367ea78c14a46427606
Boutheyna-RD <radhouane.boutheyna@gmail.com>
2024-07-17 17:52:31 +0100

    added some js functionality
    - Added search engine
    - API  Integration
    - Added Axios

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
- /*search engine*/
+  function searchCity(city){   
+    let apiKey ="43c1b8ao5e84tb2dd9c11bcc70e4f1d2";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+   /* console.log(apiKey);
+     console.log(apiUrl);*/
+    axios.get(apiUrl).then(refreshWeather);
+    
+}
+
    function handelSearch(event){
     event.preventDefault();
     let searchInput = document.getElementById('search-input');
-    /*let cityEl = document.getElementById('city');
-    cityEl.innerHTML = searchInput.value;*/
     searchCity(searchInput.value);
     }
 
